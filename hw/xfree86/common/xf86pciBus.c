bc935640aec78c96a36ca4c307ee4a836b02ff71
Ben Skeggs <bskeggs@redhat.com>
2017-04-22 02:26:28 +1000

    xfree86: use modesetting driver by default on GeForce 8 and newer
    Signed-off-by: Ben Skeggs <bskeggs@redhat.com>

--- hw/xfree86/common/xf86pciBus.c
+++ hw/xfree86/common/xf86pciBus.c
@@ -37,6 +37,9 @@
 #include <unistd.h>
 #include <X11/X.h>
 #include <pciaccess.h>
+#if defined(__linux__) || defined(__NetBSD__)
+#include <xf86drm.h>
+#endif
 #include "os.h"
 #include "Pci.h"
 #include "xf86.h"
@@ -1207,6 +1210,25 @@ xf86VideoPtrToDriverList(struct pci_device *dev, XF86MatchedDrivers *md)
         int idx = 0;
 
 #if defined(__linux__) || defined(__NetBSD__)
+        char busid[32];
+        int fd;
+
+        snprintf(busid, sizeof(busid), "pci:%04x:%02x:%02x.%d",
+                 dev->domain, dev->bus, dev->dev, dev->func);
+
+	/* 'modesetting' is preferred for GeForce 8 and newer GPUs */
+        fd = drmOpenWithType("nouveau", busid, DRM_NODE_RENDER);
+        if (fd >= 0) {
+            uint64_t args[] = { 11 /* NOUVEAU_GETPARAM_CHIPSET_ID */, 0 };
+            int ret = drmCommandWriteRead(fd, 0 /* DRM_NOUVEAU_GETPARAM */,
+                                          &args, sizeof(args));
+            drmClose(fd);
+            if (ret == 0) {
+                if (args[1] == 0x050 || args[1] >= 0x80)
+                    break;
+            }
+        }
+
         driverList[idx++] = "nouveau";
 #endif
         driverList[idx++] = "nv";
